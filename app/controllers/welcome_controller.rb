7ce70e281c42271d5695d07f36c008ebab4ecaac
Tristan Rubadeau <tristan@rubadeau.com>
2015-01-29 16:09:06 -0700

    finished Quotes Array

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+
   def index
+    @quotes_array = [
+      {author: "- Cayla Hayes", quote: "gCamp has changed my life!  It's the best tool I've ever used."},
+      {author: "- Leta Jaskolski", quote: "Before gCamp I was a disorderly slob.  Now I'm more organized than I've ever been."},
+      {author: "- Lavern Upton", quote: "Don't hesitate - sign up right now! You'll never be the same."}
+]
   end
 end
