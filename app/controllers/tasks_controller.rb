eb0ee251d373ed4f6361d532849b175a0ed5ce1b
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-06 00:07:28 -0700

    Through Task Update Method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,34 @@ class TasksController < ApplicationController
     @tasks = Task.new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save
+
+    redirect_to tasks_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
