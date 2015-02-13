5fbbb03aa6277496b698ff0a79f86cee3b365ea0
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-13 11:46:53 -0700

    CreateUsers Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212211605) do
+ActiveRecord::Schema.define(version: 20150213184537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20150212211605) do
     t.string   "description"
     t.boolean  "complete"
     t.datetime "due_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
