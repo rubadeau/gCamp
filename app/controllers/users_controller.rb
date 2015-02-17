710a80171889047a5dba263408e1b386ac0c5fb5
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-17 15:13:24 -0700

    user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,36 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "User was successfully created"
-      redirect_to users_path(@user)
+      redirect_to user_path(@user)
     else
       render :new
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
 
 private
 
