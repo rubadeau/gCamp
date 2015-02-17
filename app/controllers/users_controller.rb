772aa26533b319dc2b5bc11f250ae331ee183e48
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-17 15:31:26 -0700

    link users names to show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "User was successfully created"
-      redirect_to user_path(@user)
+      redirect_to users_path
     else
       render :new
     end
