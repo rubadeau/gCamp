dc75ef63d3e20c7ff1050b2cf2b5f58e7e459457
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-19 16:07:24 -0700

    CRUD for porjects completed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,22 +12,39 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
     if @project.save
       flash[:notice] = "Project was successfully created"
-      redirect_to projects_path
+      redirect_to project_path(@project)
     else
       render :new
     end
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      flash[:notice] = "Project was successfully updated"
+      redirect_to projects_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    project = Project.find(params[:id])
+    if project.destroy
+      flash[:notice] = "Project was successfully deleted"
+      redirect_to projects_path
+    else
+      render :edit
+    end
   end
 
   private
