ee6791a8e3be5704553c0f196d116b716d7fe8a4
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-19 14:17:34 -0700

    added Projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213184537) do
+ActiveRecord::Schema.define(version: 20150219211531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
