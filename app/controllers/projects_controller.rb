bfda54cc09148c2c97353afdfbce20e4d0f6a59b
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-19 15:30:25 -0700

    edit form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,6 +9,31 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project was successfully created"
+      redirect_to projects_path
+    else
+      render :new
+    end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:name)
+    end
 
 end
