829cd1d5d71149744913d8617013cb00092532a5
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-23 15:23:29 -0700

    user email unique validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
 
 end
