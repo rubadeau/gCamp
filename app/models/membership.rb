059af66f9c8a52781fe63770a2ae4e5512560d08
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-17 14:54:25 -0600

    added validation errors to memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,8 @@
 class Membership < ActiveRecord::Base
 
+  validates :user, presence: true
+  validates :user, uniqueness: {scope: :project_id, message: "has already been added to this project" }
+
   ROLE_MEMBER = "Member"
   ROLE_USER = "User"
   ROLE = [ROLE_MEMBER, ROLE_USER]
