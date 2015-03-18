b952024cee38611ade6b0b479d08156ae43931e6
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-18 14:26:31 -0600

    fixed routing issue when destroying task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,7 +46,7 @@ class TasksController < ApplicationController
   def destroy
     task = Task.find(params[:id])
     task.destroy
-    redirect_to tasks_path
+    redirect_to project_tasks_path
   end
 
 
