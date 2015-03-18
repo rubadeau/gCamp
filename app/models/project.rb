57d551533e02164a3dd0dd4798755763581519f3
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-18 15:47:57 -0600

    added well and btn to view and dependent destroy to model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
 
   validates :name, presence: true
 
-  has_many :tasks
-  has_many :memberships
+  has_many :tasks, dependent: :destroy
+  has_many :memberships, dependent: :destroy
 
 end
