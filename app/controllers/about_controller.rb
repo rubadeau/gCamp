fe73ecc491cef17dda1652249165f069726518f4
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-19 13:44:32 -0600

    about page counter with app wide stats

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,10 @@
 class AboutController < ApplicationController
   def index
+    @projects = Project.all
+    @tasks = Task.all
+    @memberships = Membership.all
+    @users = User.all
+    @comments = Comment.all
   end
+
 end
