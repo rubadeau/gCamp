91d282a6910f317829c57ef31b544489bcc5999b
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-23 17:06:27 -0600

    updated owner role

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,9 +3,7 @@ class Membership < ActiveRecord::Base
   validates :user, presence: true
   validates :user, uniqueness: {scope: :project_id, message: "has already been added to this project" }
 
-  ROLE_MEMBER = "Member"
-  ROLE_USER = "User"
-  ROLE = [ROLE_MEMBER, ROLE_USER]
+  ROLE = [" Member", "Owner"]
 
   belongs_to :project
   belongs_to :user
